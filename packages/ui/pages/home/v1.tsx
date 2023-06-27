941dc893a3728d455725c78154608093d1ab3237
learnuidev <learnuidev@gmail.com>
2023-06-26 23:04:39 -0400

    [RINO-003] Added Home Page v1

---
+++ packages/ui/pages/home/v1.tsx
@@ -0,0 +1,9 @@
+import { Button, Header } from 'ui'
+
+export function HomePage () {
+  return (
+    <div className='my-8 mx-16 md:mx-32 flex flex-wrap'>
+      <Header text='Mandarino' />
+    </div>
+  )
+}
